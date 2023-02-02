3bf8c578d83b134cb4412c0ebba60a71c3aee1d5
Grant Adiele <grantobioma@gmail.com>
2023-02-02 05:13:44 +0100

    version 1.0.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# Aesthetic Clock
+
+~ A modern aesthetic clock design that can be used a wallpaper.
+
+* Ability customize the theme of your clock
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        "24xl": "24rem",
+        "10xl": "10rem"
+      }
+    },
+  },
+  plugins: [],
+}
